32e44fb9749c960cea60935f67957bea14efcaca
Xu Dong <xdong14@sheffield.ac.uk>
2018-12-05 04:10:29 +0000

    degree class

--- Project Workspace/SystemDesign&SecurityProject/src/Teachers.java
+++ Project Workspace/SystemDesign&SecurityProject/src/Teachers.java
@@ -99,7 +99,18 @@ public class Teachers extends UserInterface {
 
 			}
 		});
+		classes.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				updateJSwing(main);
+				try {
+					degree(main, fr);
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 
+			}
+		});
 		// add buttons to panel
 		nav.add(profile);
 		nav.add(accounts);
@@ -133,7 +144,18 @@ public class Teachers extends UserInterface {
 		main.add(updateGrade);
 		main.add(meanGrade);
 	}
+	public void degree(JPanel main, JFrame frame) throws Exception {
 
+		JTable table = db.displayTable("Student_Class_Link");
+		JScrollPane jsp= new JScrollPane(table);
+		jsp.setSize(new Dimension(1577, 588));
+
+		
+		
+		main.setBounds(343,146, 1577, 788);
+		main.setVisible(true); 
+		main.add(jsp,BorderLayout.CENTER);
+	}
 	/**
 	 * 
 	 * @param bt
@@ -182,7 +204,8 @@ public class Teachers extends UserInterface {
 					okbtn.addActionListener(new ActionListener() {
 						@Override
 						public void actionPerformed(ActionEvent e) {
-
+							
+							
 							Integer stu = (Integer) stuIdBox.getSelectedItem();
 							int per = Integer.parseInt((String) perBox.getSelectedItem());
 
@@ -200,11 +223,20 @@ public class Teachers extends UserInterface {
 							JScrollPane jsp = new JScrollPane(meanGrade);
 							jsp.setSize(new Dimension(600, 500));
 							JLabel mean = new JLabel(
-									"Weighted mean grade for this period is : " + test + ". This student got a: ");
+									"Weighted mean grade for this period is : " + test + "");
 							mean.setBounds(30, 520, 500, 20);
-
+							JButton register = new JButton("Register a student");
+							register.setBounds(30, 550, 540, 20);
+							try {
+								
+								registerStudent(register,stu);
+							} catch (Exception e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}
 							showMeadP.add(jsp);
 							showMeadP.add(mean);
+							showMeadP.add(register);
 
 							showMead.setResizable(false);
 
@@ -227,7 +259,169 @@ public class Teachers extends UserInterface {
 			}
 		});
 	}
+	private void registerStudent(JButton remmovebt,int degId) throws Exception {
+		remmovebt.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
 
+					try {
+					JFrame regStudent=new JFrame("Register student");
+					regStudent.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+					regStudent.setLayout(null);
+					regStudent.setVisible(true);
+	
+					JPanel regStudentP=new JPanel();
+					regStudentP.setLayout(null);
+					regStudentP.setBounds(0,0,500,220);
+					regStudent.setResizable(false);
+
+					JLabel sDate = new JLabel("Start date(YYYY/MM/DD):");
+	
+			        String[] listData = new String[]{"2018", "2019", "2020", "2021","2022","2023","2024","2025"};
+			        final JComboBox<String> startYear = new JComboBox<String>(listData);
+			        startYear.setBounds(250, 20, 60, 20);
+			        String[] listData2= new String[]{"01", "02", "03", "04","05", "06", "07","08", "09", "10","11", "12"};
+			        final JComboBox<String> startMonth = new JComboBox<String>(listData2);
+			        startMonth.setBounds(310, 20, 60, 20);
+			        String[] listData3 = new String[]{"01", "02", "03", "04","05", "06", "07","08", "09", "10","11", "12"+
+			        		"13", "14", "15", "16","17", "18", "19","20", "21", "22","23", "24", "25","26", "27", "28","29", "30", "31"};
+			        final JComboBox<String> startDay = new JComboBox<String>(listData3);
+			        startDay.setBounds(370, 20, 60, 20);	
+					
+					JLabel eDate = new JLabel("End date:");
+			        String[] listData4 = new String[]{"2018", "2019", "2020", "2021","2022","2023","2024","2025"};
+			        final JComboBox<String> endYear = new JComboBox<String>(listData4);
+			        endYear.setBounds(250, 50, 60, 20);
+			        String[] listData5= new String[]{"01", "02", "03", "04","05", "06", "07","08", "09", "10","11", "12"};
+			        final JComboBox<String> endMonth = new JComboBox<String>(listData5);
+			        endMonth.setBounds(310, 50, 60, 20);
+			        String[] listData6 = new String[]{"01", "02", "03", "04","05", "06", "07","08", "09", "10","11", "12"+
+			        		"13", "14", "15", "16","17", "18", "19","20", "21", "22","23", "24", "25","26", "27", "28","29", "30", "31"};
+			        final JComboBox<String> endDay = new JComboBox<String>(listData6);
+			        endDay.setBounds(370, 50, 60, 20);
+	
+	
+					sDate.setBounds(30, 20, 220, 20);
+					eDate.setBounds(30, 50, 220, 20);
+					
+					
+					JLabel label = new JLabel("Label:");
+					label.setBounds(30, 80, 220, 20);
+	
+					String[] listData7 = new String[]{"A", "B", "C", "D","E", "F", "G","H", "I", "J","K", "L"};
+			        final JComboBox<String> labelBox = new JComboBox<String>(listData7);
+			        labelBox.setBounds(250, 80, 60, 20);
+					
+					JLabel level = new JLabel("Level of Study:");
+					level.setBounds(30, 110, 220, 20);
+			        
+			       				
+					JComboBox levelBox = db.getPBox("Level_of_Study");
+	
+					levelBox.setBounds(250, 110, 60, 20);
+					
+					JLabel degree = new JLabel("Degree class (for graduation):");
+					degree.setBounds(30, 140, 220, 20);
+					JComboBox degreeClass = db.getPBox("Degree_Class");
+					degreeClass.setBounds(250, 140, 60, 20);
+				
+					regStudentP.add(sDate);
+					regStudentP.add(degreeClass);
+					regStudentP.add(degree);
+
+					regStudentP.add(startYear);
+					regStudentP.add(startMonth);
+					regStudentP.add(startDay);
+					regStudentP.add(endYear);
+					regStudentP.add(endMonth);
+					regStudentP.add(endDay);
+					regStudentP.add(eDate);
+					regStudentP.add(labelBox);
+					regStudentP.add(label);
+					
+					regStudentP.add(levelBox);
+					regStudentP.add(level);
+					
+																								 
+				    JButton okbtn = new JButton("Progress");
+				    okbtn.setBounds(60, 170, 90, 20);
+				    JButton repeatbtn = new JButton("Repeat");
+				    repeatbtn.setBounds(160, 170, 90, 20);
+				    JButton graduebtn = new JButton("Graduate");
+				    graduebtn.setBounds(260, 170, 90, 20);
+				    JButton failbtn = new JButton("Fail");
+				    failbtn.setBounds(360, 170, 90, 20);
+				    
+				    regStudentP.add(okbtn);
+				    regStudentP.add(repeatbtn);
+				    regStudentP.add(graduebtn);
+				    regStudentP.add(failbtn);
+
+				    regStudent.setLocation(900,500);
+				    regStudent.setSize(500,240);
+				    regStudent.setVisible(true);
+				    regStudent.add(regStudentP);
+				    okbtn.addActionListener(new ActionListener() {
+						@Override
+						public void actionPerformed(ActionEvent e) {
+	
+							
+							String startDate = (String) startYear.getSelectedItem()+"-"+(String) startMonth.getSelectedItem()+"-"+startDay.getSelectedItem();
+							String endDate = (String) endYear.getSelectedItem()+"-"+(String) endMonth.getSelectedItem()+"-"+endDay.getSelectedItem();
+							char label = ((String) labelBox.getSelectedItem()).charAt(0);
+							String level = (String) levelBox.getSelectedItem();
+							System.out.print(startDate);
+							db.registerStudent(startDate, endDate,label, level,degId, "pass");
+							System.out.println("Success");
+							regStudent.dispose();
+								
+								
+						}
+					});
+				    repeatbtn.addActionListener(new ActionListener() {
+						@Override
+						public void actionPerformed(ActionEvent e) {
+							String startDate = (String) startYear.getSelectedItem()+"-"+(String) startMonth.getSelectedItem()+"-"+startDay.getSelectedItem();
+							String endDate = (String) endYear.getSelectedItem()+"-"+(String) endMonth.getSelectedItem()+"-"+endDay.getSelectedItem();
+							char label = ((String) labelBox.getSelectedItem()).charAt(0);
+							String level = (String) levelBox.getSelectedItem();
+							db.registerStudent(startDate, endDate,label, level,degId,"repeat");
+							regStudent.dispose();
+						}
+					});
+				    graduebtn.addActionListener(new ActionListener() {
+						@Override
+						public void actionPerformed(ActionEvent e) {
+							String classID = (String)degreeClass.getSelectedItem();
+							db.graduate(degId,classID);
+							regStudent.dispose();
+						}
+					});
+				    failbtn.addActionListener(new ActionListener() {
+						@Override
+						public void actionPerformed(ActionEvent e) {
+//							String startDate = (String) startYear.getSelectedItem()+"-"+(String) startMonth.getSelectedItem()+"-"+startDay.getSelectedItem();
+//							String endDate = (String) endYear.getSelectedItem()+"-"+(String) endMonth.getSelectedItem()+"-"+endDay.getSelectedItem();
+//							char label = ((String) labelBox.getSelectedItem()).charAt(0);
+//							String level = (String) levelBox.getSelectedItem();
+//							db.registerStudent(startDate, endDate,label, level,degId,"fail");
+//							regStudent.dispose();
+							regStudent.dispose();
+						}
+					});
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+				
+				
+				
+			
+		});
+	
+	}
 	/**
 	 * 
 	 * @param bt
